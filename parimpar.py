deaabe989bb8f1483be57e83ea97bd7680e8c953
AlexandreMeirinhosShark <shark05980@sharkcoders.pt>
2024-11-02 11:54:48 +0000

    Add files via upload

---
+++ parimpar.py
@@ -0,0 +1,8 @@
+num = int(input("Número: "))
+if num != 0:
+    if num % 2 == 1:
+        print("O númeor é ímpar")
+    else:
+        print("O número é par")
+else:
+    print("0 não é par nem ímpar.")
